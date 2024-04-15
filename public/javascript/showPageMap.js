b175e52ff853ecf4e7a97961b748bf8fb712b0b3
123Dheeraj123 <dheerajvats95@gmail.com>
2024-04-15 10:08:21 +0530

    need some configuration

---
+++ public/javascript/showPageMap.js
@@ -0,0 +1,26 @@
+
+const campground1 = campground
+
+
+
+
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+    center: campground1.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10 // starting zoom
+});
+
+map.addControl(new mapboxgl.NavigationControl());
+
+
+new mapboxgl.Marker()
+    .setLngLat(campground1.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground1.title}</h3><p>${campground1.location}</p>`
+            )
+    )
+    .addTo(map)
